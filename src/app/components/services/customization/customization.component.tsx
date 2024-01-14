0941032d0b7d154cd5d03dfe1ded1c18ca845ea1
pablomartan <pablomartan@kolabnow.com>
2024-01-14 16:17:48 +0100

    feat: adapt customization component to be able to show dot on big screens

--- src/app/components/services/customization/customization.component.tsx
+++ src/app/components/services/customization/customization.component.tsx
@@ -6,9 +6,13 @@ import './customization.style.scss';
 export const Customization: React.FC<ICustomization> = ({title, subtitle, url}) => {
     return(
         <div className='Customization'>
+            <div>
             <h4 className="Customization__title">{title}</h4>
             <h5 className="Customization__subtitle">{subtitle}</h5>
-            <Button url={url} />
+            </div>
+            <div>
+            <Button url={url} className="Customization__button" />
+            </div>
         </div>
     )
 };
