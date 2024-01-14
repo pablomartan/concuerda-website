c86cd1c62aa7cb2f22ed66fa0432a316c5a41e4c
pablomartan <pablomartan@kolabnow.com>
2024-01-14 16:18:07 +0100

    style: run linter on customization component

--- src/app/components/services/customization/customization.component.tsx
+++ src/app/components/services/customization/customization.component.tsx
@@ -3,15 +3,15 @@ import { Button } from '../../button/button.component';
 import { ICustomization } from '../../../../custom';
 import './customization.style.scss';
 
-export const Customization: React.FC<ICustomization> = ({title, subtitle, url}) => {
-    return(
+export const Customization: React.FC<ICustomization> = ({ title, subtitle, url }) => {
+    return (
         <div className='Customization'>
             <div>
-            <h4 className="Customization__title">{title}</h4>
-            <h5 className="Customization__subtitle">{subtitle}</h5>
+                <h4 className="Customization__title">{title}</h4>
+                <h5 className="Customization__subtitle">{subtitle}</h5>
             </div>
             <div>
-            <Button url={url} className="Customization__button" />
+                <Button url={url} className="Customization__button" />
             </div>
         </div>
     )
