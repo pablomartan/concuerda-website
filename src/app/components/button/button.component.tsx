227a678c3babac6364935212d43beca9936293f0
pablomartan <pablomartan@kolabnow.com>
2024-01-14 16:16:03 +0100

    feat: add className props to Button components

--- src/app/components/button/button.component.tsx
+++ src/app/components/button/button.component.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './button.style.scss';
 
-export const Button: React.FC<{url: string}> = ({url}) => {
+export const Button: React.FC<{url: string; className?: string;}> = ({url, className}) => {
     return(
-        <button className='Button'>
+        <button className={"Button ".concat(className ?? '')}>
             <a href={url}>ver más</a>
         </button>
     );
