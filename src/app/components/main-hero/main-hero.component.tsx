a64425eb5ce36199db0bdd7416363c43dcf7bb1e
pablomartan <pablomartan@kolabnow.com>
2024-01-28 12:56:22 +0100

    refactor: move main-hero bg setting to route style file

--- src/app/components/main-hero/main-hero.component.tsx
+++ src/app/components/main-hero/main-hero.component.tsx
@@ -4,27 +4,12 @@ import "./main-hero.style.scss";
 export type MainHeroProps = {
   hook: String;
   subtext: String;
-  currentComponentClass:
-    | "Home"
-    | "Weddings"
-    | "Events"
-    | "Infinity"
-    | "CMA"
-    | "Customization"
-    | "Videos"
-    | "Contact"
-    | "Projects"
-    | "NotFound";
 };
 
-export const MainHero: FC<MainHeroProps> = ({
-  hook,
-  subtext,
-  currentComponentClass,
-}) => {
+export const MainHero: FC<MainHeroProps> = ({ hook, subtext }) => {
   return (
     <div className="MainHero">
-      <div className={`MainHero__bg ${currentComponentClass}`} />
+      <div className="MainHero__bg" />
       <div className="MainHero__text">
         <h2 className="MainHero__hook">{hook}</h2>
         <h3 className="MainHero__subtext">{subtext}</h3>
