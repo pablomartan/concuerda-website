7146b07756bfca4e742283cd0c2950088d178827
pablomartan <pablomartan@kolabnow.com>
2024-01-28 13:00:07 +0100

    style: run prettier in header component and services style

--- src/app/components/header/header.component.tsx
+++ src/app/components/header/header.component.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Menu } from '../menu/menu.component'
-import './header.style.scss';
-import logo from '../../../assets/img/black_logo.png';
+import React from "react";
+import { Menu } from "../menu/menu.component";
+import "./header.style.scss";
+import logo from "../../../assets/img/white_logo.png";
 
 export const Header = () => {
-    return(
-        <header className="Header">
-            <img className="Header__logo" src={logo} alt="" />
-            <Menu />
-        </header>
-    )
+  return (
+    <header className="Header">
+      <img className="Header__logo" src={logo} alt="" />
+      <Menu />
+    </header>
+  );
 };
