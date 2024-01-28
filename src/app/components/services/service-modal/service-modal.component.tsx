c27dfa82843ea67da434254167a24c866a81d5b8
pablomartan <pablomartan@kolabnow.com>
2024-01-28 12:55:16 +0100

    refactor: service modal premium customizations

--- src/app/components/services/service-modal/service-modal.component.tsx
+++ src/app/components/services/service-modal/service-modal.component.tsx
@@ -1,6 +1,12 @@
 import React, { FC } from "react";
-import { Customization } from "../customization/customization.component";
-import { useWeddingsServiceModalsData } from "../../../hooks/useData";
+import {
+  Customization,
+  CustomizationType,
+} from "../customization/customization.component";
+import {
+  useWeddingsServiceModalsData,
+  useEventsServiceModalData,
+} from "../../../hooks/useData";
 import "./service-modal.style.scss";
 import { capitalize } from "../../../utils/utils";
 
@@ -9,10 +15,25 @@ type ServiceModalKeyType =
   | "civil"
   | "cocktail"
   | "dinner"
-  | "party";
+  | "party"
+  | "corporate"
+  | "custom";
+
+type ServiceModalType = {
+  hero: string;
+  title: string;
+  subtitle: string;
+  customizations: CustomizationType[];
+  premiumCustomization?: CustomizationType;
+  footer: { url: string; text: string };
+};
 
 export const ServiceModal: FC<{ modal: ServiceModalKeyType }> = ({ modal }) => {
-  const modalCustomizations = useWeddingsServiceModalsData();
+  const modalCustomizations = {
+    ...useWeddingsServiceModalsData(),
+    ...useEventsServiceModalData(),
+  };
+
   const {
     hero,
     title,
@@ -20,7 +41,7 @@ export const ServiceModal: FC<{ modal: ServiceModalKeyType }> = ({ modal }) => {
     customizations,
     premiumCustomization,
     footer: { url: footerLink, text: footerText },
-  } = modalCustomizations[modal];
+  } = modalCustomizations[modal] as ServiceModalType;
 
   return (
     <div className="ServiceModal">
@@ -45,14 +66,16 @@ export const ServiceModal: FC<{ modal: ServiceModalKeyType }> = ({ modal }) => {
         {customizations.map((customization) => {
           return <Customization {...customization} />;
         })}
-        <div className="ServiceModal__body__premium-customization">
-          <video
-            className={"ServiceModal__body__premium-customization__banner"}
-            src={premiumCustomization["banner"]}
-            autoPlay={true}
-          />
-          <Customization {...premiumCustomization} />
-        </div>
+        {premiumCustomization ? (
+          <div className="ServiceModal__body__premium-customization">
+            <video
+              className={"ServiceModal__body__premium-customization__banner"}
+              src={premiumCustomization["banner"]}
+              autoPlay={true}
+            />
+            <Customization {...premiumCustomization} />
+          </div>
+        ) : null}
       </main>
       <footer className="ServiceModal__footer">
         <a className="ServiceModal__footer__link" href={footerLink}>
