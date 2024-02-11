62f9f91bbff4b597be61d31acb5e0cfa614a4ff7
pablomartan <pablomartan@kolabnow.com>
2024-02-11 16:27:24 +0100

    refactor: remove anchor by default from button

--- src/app/components/button/button.component.tsx
+++ src/app/components/button/button.component.tsx
@@ -2,11 +2,11 @@ import React, { PropsWithChildren } from "react";
 import "./button.style.scss";
 
 export const Button: React.FC<
-  PropsWithChildren<{ url: string; className?: string }>
-> = ({ url, className, children }) => {
+  PropsWithChildren<{ url: string; className?: string; target?: string }>
+> = ({ className, children }) => {
   return (
     <button className={"Button".concat(` ${className}` ?? "")}>
-      <a href={url}>{children || "ver más"}</a>
+      {children}
     </button>
   );
 };
