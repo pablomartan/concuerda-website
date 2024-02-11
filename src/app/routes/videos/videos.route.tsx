e41ab48e65828ab69bd758fcb80b4a38db91afc2
pablomartan <pablomartan@kolabnow.com>
2024-02-11 17:00:57 +0100

    feat: add header and footer to Videos route

--- src/app/routes/videos/videos.route.tsx
+++ src/app/routes/videos/videos.route.tsx
@@ -1,9 +1,18 @@
 import { FC } from "react";
 
+import { Header } from "../../components/header/header.component";
+import { Footer } from "../../components/footer/footer.component";
+
 import "./videos.style.scss";
 
 const Videos: FC = () => {
-  return <></>;
+  return (
+    <div className="Videos">
+      <Header />
+      <div className="Videos__body-placeholder" />
+      <Footer />
+    </div>
+  );
 };
 
 export default Videos;
