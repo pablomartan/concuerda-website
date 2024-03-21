5dcb225f22e75bdbf11ef5d511f84660f210b9e4
pablomartan <pablomartan@kolabnow.com>
2024-03-21 20:13:18 +0100

    feat: add infinity style prop to button

--- src/app/components/button/button.component.tsx
+++ src/app/components/button/button.component.tsx
@@ -3,14 +3,16 @@ import "./button.style.scss";
 
 export const Button: React.FC<
   PropsWithChildren<{
-    style?: "black" | "yellow" | "transparent";
+    style?: "black" | "yellow" | "transparent" | "infinity";
     size?: "small";
   }>
 > = ({ style, size, children }) => {
   const className = " " + (style ?? "") + " " + (size ?? "");
 
   return (
-    <button className={"Button".concat(className.length > 2 ? className : "")}>
+    <button
+      className={"Button".concat(className.length > 2 ? className : "").trim()}
+    >
       {children}
     </button>
   );
